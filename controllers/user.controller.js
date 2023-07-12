0538c4e2e58c970897bc7b09c013c0df77800ff7
Your Name <dinca.toni@gmail.com>
2023-07-12 03:00:27 +0300

    login and register response changed to json

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,18 +1,21 @@
 let User = require("../models/user.model");
-const {generateToken, deleteToken} = require('../utils/generateToken');
+const { generateToken, deleteToken } = require("../utils/generateToken");
 
 const authUser = async (req, res, next) => {
   try {
-    
-    const {email, password} = req.body;
-    
-    const user = await User.findOne({email: email});
-    if(user && (await user.matchPasswords(password))){
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (user && (await user.matchPasswords(password))) {
       generateToken(res, user._id);
-        res.status(201).send("Utilizator autentificat");
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
     } else {
-        res.status(401) ;
-        throw new Error('Email sau parolă greșite');
+      res.status(401);
+      throw new Error("Email sau parolă greșite");
     }
   } catch (error) {
     return next(error);
@@ -29,14 +32,18 @@ const registerUser = async (req, res, next) => {
       res.status(400);
       throw new Error("Utilizator deja existent");
     }
-    
-    const newUser = await User.create({name, email, password});
-    if(newUser){
+
+    const newUser = await User.create({ name, email, password });
+    if (newUser) {
       generateToken(res, newUser._id);
-        res.status(201).send("Utilizator întrgistrat");
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      });
     } else {
-        res.status(400);
-        throw new Error('Date invalide')
+      res.status(400);
+      throw new Error("Date invalide");
     }
   } catch (error) {
     return next(error);
@@ -56,9 +63,9 @@ const logoutUser = async (req, res, next) => {
 const getUserProfile = async (req, res, next) => {
   try {
     const user = {
-      _id : req.user._id,
-      email : req.user.email,
-      name : req.user.name 
+      _id: req.user._id,
+      email: req.user.email,
+      name: req.user.name,
     };
     res.status(200).json(user);
   } catch (error) {
@@ -70,24 +77,24 @@ const updateUserProfile = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id);
 
-    if(user){
+    if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
-      
-      if (req.body.password){
+
+      if (req.body.password) {
         user.password = req.body.password;
       }
 
       await user.save();
 
       res.status(200).json({
-        _id : user._id,
-      email : user.email,
-      name : user.name 
+        _id: user._id,
+        email: user.email,
+        name: user.name,
       });
-    }else {
+    } else {
       res.staus(404);
-      throw new Error('Utilizator inexistent');
+      throw new Error("Utilizator inexistent");
     }
   } catch (error) {
     return next(error);
